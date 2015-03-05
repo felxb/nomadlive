1323bdd619bf28fdbff962bf1cc76efdfe2620ee
felxb <phelix.b@gmail.com>
2015-03-05 13:33:32 -0500

    improved responsiveness

--- searchform.php
+++ searchform.php
@@ -1,6 +1,8 @@
 <!-- search -->
-<form class="search-form" method="get" action="<?php echo home_url(); ?>" role="search">
-	<span class="icon"><i class="fa fa-search"></i></span>
-	<input class="search-input" type="search" name="s" placeholder="<?php echo _e('Search NOMADlive.tv', 'nomadlive' ); ?>">
-</form>
+<div class="search-form-container">
+	<form class="nomad-search-form" method="get" action="<?php echo home_url(); ?>" role="search">
+		<span class="icon"><i class="fa fa-search"></i></span>
+		<input class="search-input" type="search" name="s" placeholder="<?php echo _e('Search NOMADlive.tv', 'nomadlive' ); ?>">
+	</form>
+</div>
 <!-- /search -->
