29e6c8bdf1f6c8b8a09261d50b71b1b4ea06d64d
felxb <phelix.b@gmail.com>
2015-03-08 14:23:29 -0400

    livecast thumbnail

--- loop.php
+++ loop.php
@@ -5,7 +5,14 @@
         <?php if( $displayHomeBool ){?>
         <div class="thumb col span_1_of_4 non-selectable" id="thumb0">
             <a href="<?php bloginfo("url");?>?channel=<?php $tag = get_queried_object(); echo $tag->slug; ?>" data-link="<?php bloginfo('url');?>" data-type="ustream" data-content="<iframe width='660' height='387' src='http://www.ustream.tv/embed/18155672?ub=85a901&amp;lc=85a901&amp;oc=ffffff&amp;uc=ffffff&amp;v=3&amp;wmode=direct&amp;autoplay=true' scrolling='no' frameborder='0' style='border: 0px none transparent;'></iframe>">
-                <img src="<?php echo get_template_directory_uri();?>/inc/img/to-live.png" alt="Livecast"/>
+                <div class="video-title-container nomad-live-title non-selectable">
+                    <div class="video-title">
+                        <?php bloginfo('name'); echo " | "; bloginfo('description');?>
+                    </div>
+                </div>
+                <div class="thumb-img-container non-selectable">
+                    <img src="<?php echo get_template_directory_uri();?>/inc/img/to-live.png" alt="Livecast"/>
+                </div>
             </a>
         </div>
         <?php } ?>
@@ -15,8 +22,15 @@
         <?php $displayHomeBool = get_field('display_livestream_on_story_page', $queried_object);?>
         <?php if( $displayHomeBool ){?>
         <div class="thumb col span_1_of_4 non-selectable" id="thumb0">
-            <a href="<?php bloginfo("url");?>?story=<?php $cat = get_queried_object(); echo $cat->slug; ?>" data-link="<?php bloginfo('url');?>" data-type="ustream" data-content="<iframe width='660' height='387' src='http://www.ustream.tv/embed/18155672?ub=85a901&amp;lc=85a901&amp;oc=ffffff&amp;uc=ffffff&amp;v=3&amp;wmode=direct&amp;autoplay=true' scrolling='no' frameborder='0' style='border: 0px none transparent;'></iframe>">
-                <img src="<?php echo get_template_directory_uri();?>/inc/img/to-live.png" alt="Livecast"/>
+            <a href="<?php bloginfo("url");?>?channel=<?php $tag = get_queried_object(); echo $tag->slug; ?>" data-link="<?php bloginfo('url');?>" data-type="ustream" data-content="<iframe width='660' height='387' src='http://www.ustream.tv/embed/18155672?ub=85a901&amp;lc=85a901&amp;oc=ffffff&amp;uc=ffffff&amp;v=3&amp;wmode=direct&amp;autoplay=true' scrolling='no' frameborder='0' style='border: 0px none transparent;'></iframe>">
+                <div class="video-title-container nomad-live-title non-selectable">
+                    <div class="video-title">
+                        <?php bloginfo('name'); echo " | "; bloginfo('description');?>
+                    </div>
+                </div>
+                <div class="thumb-img-container non-selectable">
+                    <img src="<?php echo get_template_directory_uri();?>/inc/img/to-live.png" alt="Livecast"/>
+                </div>
             </a>
         </div>
         <?php } ?>
@@ -25,8 +39,15 @@
 <?php } else if (is_search()) {?>
 <?php } else {?>
         <div class="thumb col span_1_of_4 non-selectable" id="thumb0">
-            <a href="<?php bloginfo("url");?>" data-link="<?php bloginfo('url');?>" data-type="ustream" data-content="<iframe width='660' height='387' src='http://www.ustream.tv/embed/18155672?ub=85a901&amp;lc=85a901&amp;oc=ffffff&amp;uc=ffffff&amp;v=3&amp;wmode=direct&amp;autoplay=true' scrolling='no' frameborder='0' style='border: 0px none transparent;'></iframe>">
-                <img src="<?php echo get_template_directory_uri();?>/inc/img/to-live.png" alt="Livecast"/>
+            <a href="<?php bloginfo("url");?>?channel=<?php $tag = get_queried_object(); echo $tag->slug; ?>" data-link="<?php bloginfo('url');?>" data-type="ustream" data-content="<iframe width='660' height='387' src='http://www.ustream.tv/embed/18155672?ub=85a901&amp;lc=85a901&amp;oc=ffffff&amp;uc=ffffff&amp;v=3&amp;wmode=direct&amp;autoplay=true' scrolling='no' frameborder='0' style='border: 0px none transparent;'></iframe>">
+                <div class="video-title-container nomad-live-title non-selectable">
+                    <div class="video-title">
+                        <?php bloginfo('name'); echo " | "; bloginfo('description');?>
+                    </div>
+                </div>
+                <div class="thumb-img-container non-selectable">
+                    <img src="<?php echo get_template_directory_uri();?>/inc/img/to-live.png" alt="Livecast"/>
+                </div>
             </a>
         </div>
 <?php } ?>
